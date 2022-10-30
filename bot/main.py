a800d5fd2e40f284ad609a9c884c78258a9d73a4
Tony Toibero <tony.toibero@yahoo.com>
2022-10-30 00:18:28 -0400

    Bot should be less shit now

--- bot/main.py
+++ bot/main.py
@@ -4,10 +4,18 @@ import os
 import discord
 import re
 import threading, time
+import asyncio
 
 class CustomClient(discord.Client):
 
-    def handle_message(self, message):
+    async def send_reminder(self, delayIn, message):
+        await asyncio.sleep(delay=delayIn)
+        await message.channel.send(":3")
+
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message):
         print('welcome')
         if message.author == client.user:
             return
@@ -22,17 +30,11 @@ class CustomClient(discord.Client):
                     wait_time = wait_time * 60
                 if(results.group(2) == "hours"):
                     wait_time = wait_time * 60 * 60
+        asyncio.create_task(self.send_reminder(wait_time, message))
             
-            time.sleep(wait_time)
-            print(':3')
+            
 
-    async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
 
-    async def on_message(self, message):
-        thread = threading.Thread(target=self.handle_message, args=(message,))
-        thread.start()
-        
 if __name__ == '__main__':
     client = CustomClient(intents=discord.Intents.all())
     client.run(os.getenv('DISCORD_TOKEN'))
